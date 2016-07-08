5bbcd5127ce84d1f8e5f54b4924cee488a7827bc
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-07-07 22:38:35 -0400

    debug...

--- intensity_beam.cpp
+++ intensity_beam.cpp
@@ -30,8 +30,8 @@ struct intensity_beam : public vdif_processor {
     string acqdir;
 
     bool initialized;
-    int64_t first_timestamp;
-    int64_t last_fileid;
+    int64_t initial_t0;
+    int64_t next_t0;
     std::unique_ptr<ch_frb_io::intensity_hdf5_ofile> curr_ofile;
     
     ssize_t nt_alloc;
@@ -48,32 +48,38 @@ struct intensity_beam : public vdif_processor {
 
 intensity_beam::intensity_beam(const string &acqdir_) :
     vdif_processor("intensity_beam", true),
-    acqdir(acqdir_), initialized(false), first_timestamp(0), nt_alloc(0)
+    acqdir(acqdir_), initialized(false), initial_t0(0), next_t0(0), nt_alloc(0)
 { }
 
 
 // virtual override
 void intensity_beam::process_chunk(const shared_ptr<assembled_chunk> &a)
 {
-    ssize_t nt_chunk_hires = a->nt;
-    ssize_t nt_chunk_lores = nt_chunk_hires / nt_downsample;
     const int nfreq = constants::chime_nfreq;
 
+    ssize_t t0_chunk_hires = a->t0;
+    ssize_t nt_chunk_hires = a->nt;
+    ssize_t t0_chunk_lores = t0_chunk_hires / nt_downsample;
+    ssize_t nt_chunk_lores = nt_chunk_hires / nt_downsample;
+
+    if ((t0_chunk_hires <= 0) || (t0_chunk_hires != nt_downsample * t0_chunk_lores))
+	throw runtime_error("intensity_beam: internal error: bad timestamp");
     if ((nt_chunk_hires <= 0) || (nt_chunk_hires != nt_downsample * nt_chunk_lores))
 	throw runtime_error("intensity_beam: internal error: bad assembled_chunk size");
 
     if (!initialized) {
-	this->first_timestamp = a->t0;
-	this->last_fileid = -1;
+	this->initial_t0 = t0_chunk_lores;
+	this->next_t0 = t0_chunk_lores;
 	this->initialized = true;
     }
 
+    if (t0_chunk_lores < next_t0)
+	throw runtime_error("intensity_beam: internal error: bad timestamp");
+
     if (!curr_ofile) {
-	int64_t fileid = (a->t0 - first_timestamp) / 400000;
+	ssize_t fileid = (t0_chunk_lores - initial_t0) / 1000;
 	if ((fileid < 0) || (fileid > 99999999))
-	    throw runtime_error("intensity_beam: internal error: bad timestamp");
-	if (fileid <= last_fileid)
-	    throw runtime_error("intensity_beam: internal error: bad timestamp");
+	    throw runtime_error("intensity_beam: internal error: bad fileid");
 
 	// Following a convention used throughout CHIMEFRB, we use a filename
 	// of the form NNNNNNNN.h5, where N=[0,9].
@@ -86,12 +92,11 @@ void intensity_beam::process_chunk(const shared_ptr<assembled_chunk> &a)
 	double freq0_MHz = 800.0;
 	double freq1_MHz = 400.0;
 	double dt_sample = 2.56e-6 * nt_downsample;
-	ssize_t ipos0 = a->t0;
-	double time0 = a->t0 * dt_sample;
+	ssize_t ipos0 = t0_chunk_lores;
+	double time0 = t0_chunk_lores * dt_sample;
 	int bitshuffle = 3;    // mandatory compression
 
 	this->curr_ofile = make_unique<ch_frb_io::intensity_hdf5_ofile> (filename, nfreq, pol, freq0_MHz, freq1_MHz, dt_sample, ipos0, time0, bitshuffle);
-	this->last_fileid = fileid;
     }
 
     if (nt_alloc < nt_chunk_lores) {
@@ -131,7 +136,8 @@ void intensity_beam::process_chunk(const shared_ptr<assembled_chunk> &a)
 	}
     }
 
-    curr_ofile->append_chunk(nt_chunk_lores, intensityp, weightsp, a->t0);
+    curr_ofile->append_chunk(nt_chunk_lores, intensityp, weightsp, t0_chunk_lores);
+    this->next_t0 = t0_chunk_lores + nt_chunk_lores;
 
     if (curr_ofile->curr_nt >= nt_maxfile)
 	this->curr_ofile = unique_ptr<ch_frb_io::intensity_hdf5_ofile> ();  // emptying this pointer flushes and writes file
